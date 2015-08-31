68cdd9c997576232dac2944f4047b92929e16243
Harry Xue <wxue@college.harvard.edu>
2015-08-31 14:40:21 -0400

    Reworked directory structure

--- js/popup.js
+++ js/popup.js
@@ -2,9 +2,20 @@ function getGoalTerm(){
     // Cancel form submit
     event.preventDefault();
 
+    // URL to generate random Wikipedia page
+    var url = "https://en.wikipedia.org/wiki/Special:Random"
+
     // Get the goal term that the user has entered
     var goalTerm = document.getElementById('goalterm').value;
     console.log(goalTerm);
+
+    // Redirect to random wikiipedia page
+    // TODO: Ensure that the user stays only within Wikipedia?
+    chrome.tabs.update(null, {url:url});
+
+    // Set badge text to 0
+    // TODO: Set tabId as well?
+    chrome.browserAction.setBadgeText({text:"0"});
 }
 
 // When the popup has loaded
