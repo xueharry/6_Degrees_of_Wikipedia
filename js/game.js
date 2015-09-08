26a7fa4904e288240ddd0973ee2a4bfff8c0f358
Harry Xue <wxue@college.harvard.edu>
2015-09-07 21:57:17 -0400

    Added page view google analytics

--- js/game.js
+++ js/game.js
@@ -23,3 +23,17 @@ function resetGame() {
     // the popup is closed and reopened again
     window.close();
 }
+
+// Google Analytics
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-67282294-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+    var ga = document.createElement('script');
+    ga.type = 'text/javascript'; 
+    ga.async = true;
+    ga.src = 'https://ssl.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; 
+    s.parentNode.insertBefore(ga, s);
+})();
